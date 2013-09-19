48d872d825bb46889db31796d7cc2824290fa4d2
Kyle Killion <killion@mac.com>
2013-09-19 09:29:53 -0700

    Fix time parsing

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,2 @@
 Time::DATE_FORMATS[:local] = Proc.new { |time| time.stamp("3:21 pm") }
-Time::DATE_FORMATS[:europian] = Proc.new { |time| time.stamp("08:21") }
+Time::DATE_FORMATS[:europian] = Proc.new { |time| time.stamp("14:21") }
